4786c17ac5326c8a8bdff2b518883b9959579508
gab2691 <gabriel_bartholo_batista@hotmail.com>
2019-08-12 09:34:39 -0300

    Update LordVaderReport.sql

--- src/main/java/com/config/mobiAuto/LordVaderReport.sql
+++ src/main/java/com/config/mobiAuto/LordVaderReport.sql
@@ -1,4 +1,9 @@
-select * 
+select OrderID,
+       CustomerID,
+       ProductName,
+       UnitPrice,
+       Quantity
+       
 from pedido 
 where CustomerName = 'DeathStar'
 order by OrderID, CustomerID, ProductName, UnitPrice, Quantity;
